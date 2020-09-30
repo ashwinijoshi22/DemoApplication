a1b4c5e8db9be0146100d3a75406f6defcc80083
Ashwini Bhat <ashwini.bhat@happiestminds.com>
2020-09-30 20:28:29 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionContentComponent } from './components/question-container/question-content/question-content.component';
+import { QuestionFooterComponent } from './components/question-container/question-footer/question-footer.component';
+import { QuestionContainerComponent } from './components/question-container/question-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionContentComponent,
+    QuestionFooterComponent,
+    QuestionContainerComponent
   ],
   imports: [
     BrowserModule,
